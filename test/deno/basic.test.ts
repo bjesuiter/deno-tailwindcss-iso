83b49dd85a1caffc4ed214c6f8f11b4f298060dd
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-14 11:40:23 +0200

    add test file for deno with first test - error:
    error: Relative import path "wbg" not prefixed with / or ./ or ../
        at file:///Users/bjesuiter/Develop/bjesuiter/deno-tailwindcss-iso/src/deno/oxide/tailwindcss_oxide_bg.wasm:1:360

---
+++ test/deno/basic.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "jsr:@std/assert";
+import { getTailwindClasses } from "../../src/deno/get-tailwind-classes.js";
+
+Deno.test("getTailwindClasses", async () => {
+    const content = '<div class="p-4 bg-blue-500 text-white">Hello</div>';
+    const classes = await getTailwindClasses({ content });
+    assertEquals(classes, ["p-4", "bg-blue-500", "text-white"]);
+});
